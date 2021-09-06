fae2448b339e157aeea55950dafe576cd612e9ca
Mohitraj Shah <shahmohit50@gmail.com>
2021-09-06 09:27:10 +0530

    Min

---
+++ min.java
@@ -0,0 +1,15 @@
+package Cal;
+
+public class add {
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 20;
+		
+		if(a>b){
+            System.out.println("Min number is "+b);
+        }else{
+            System.out.println("Min number is "+a;
+        }
+		
+	}
+}
