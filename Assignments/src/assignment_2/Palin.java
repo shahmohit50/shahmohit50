c689408909254c693b801b46e6c0c5e293c19d14
Mohitraj Shah <shahmohit50@gmail.com>
2021-09-21 09:10:08 +0530

    Assignment 2

--- Assignments/src/assignment_2/Palin.java
+++ Assignments/src/assignment_2/Palin.java
@@ -2,6 +2,21 @@ package assignment_2;
 
 public class Palin {
 	public static void main(String[] args) {
-		
+		String str = "MADAM";
+		int a = str.length();
+		boolean flag = false;
+		for(int i =0; i<a/2;i++) {
+			if(str.charAt(i) == str.charAt(a-i-1)) {
+				continue;
+			}else {
+				flag = true;
+				break;
+			}	
+		}
+		if(flag) {
+			System.out.println("Not a Palindrom");
+		}else {
+			System.out.println("Palindrom");
+		}
 	}
 }
