ad9611707965c2da3acfcccd79070700b8891baf
Mohitraj Shah <shahmohit50@gmail.com>
2021-10-05 10:09:10 +0530

    Assignment

---
+++ Assignments/src/assignment/RemoveElement.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class RemoveElement {
+	public static void main(String[] args) {
+		int arr[] = {1,2,4,5,7};
+		int k =5;
+		
+		for(int i=0;i<arr.length;i++) {
+			if(k == arr[i]) {
+				for(int j =i+1;j<arr.length;j++,i++) {
+					arr[i] = arr[j];
+				}
+				break;
+			}
+		}
+		
+		for(int i=0;i<arr.length-1;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
