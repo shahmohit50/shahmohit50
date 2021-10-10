53c622e7194f38ab98fa299b971e17fdaf23d164
Mohitraj Shah <shahmohit50@gmail.com>
2021-10-10 20:33:36 +0530

    10/10/2021

--- Assignments/src/assignment/Array.java
+++ Assignments/src/assignment/Array.java
@@ -1,7 +1,5 @@
 package assignment;
 
-import java.util.*;
-
 public class Array {
 	public static void main(String[] args) {
 		/*
@@ -9,8 +7,18 @@ public class Array {
 		 * class should be named Solution.
 		 */
 		int arr[] = {1,2,3,4,5};
-		int arr2[] = {4,5};
-		
+		int arr2[] = {6,4,5,7};
+		int ii=0;
+		for(int i=0; i< arr2.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if(arr[j] == arr2[i]) {
+					if(ii>0) System.out.print(",");
+					System.out.print(arr[j]);
+					ii++;
+					break;
+				}
+			}
+		}
 		
 	}
 
